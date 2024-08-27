963221764964b1db19c36d001d2925974440ba87
Harsh Ruhil <ruhilharsh8@gmail.com>
2024-08-27 12:53:38 +0530

    Add files via upload

---
+++ opencv7.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+#note: openCV by default reads images in BGR scale, but if the same image is accessed using some other library,say matplotlib
+#then that image is read in RGB scale
+
+
+img = cv.imread('../Resources/Photos/park.jpg')
+cv.imshow('Park', img)
+
+# plt.imshow(img)
+# plt.show()
+
+# BGR to Grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# BGR to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow('HSV', hsv)
+
+# BGR to L*a*b
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow('LAB', lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('RGB', rgb)
+
+# HSV to BGR
+lab_bgr = cv.cvtColor(lab, cv.COLOR_LAB2BGR)
+cv.imshow('LAB --> BGR', lab_bgr)
+
+cv.waitKey(0)
